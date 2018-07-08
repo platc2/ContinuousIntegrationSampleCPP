6dea656b4b7825f682751efed3425aa87231a406
Casimir Platzer <casimirbenjamin.platzer@students.bfh.ch>
2018-07-08 10:36:39 +0200

    - Modified Travis CI configuration for OSX, updated OSX image - Fixed main.cpp not including required stdlib.h header for EXIT_SUCCESS

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>  // EXIT_SUCCESS, EXIT_FAILURE
 
 
 #define LOG(message) do { std::cout << "[" << __FILE__ << ":" << __LINE__ << "] " << message << std::endl; } while(0)
